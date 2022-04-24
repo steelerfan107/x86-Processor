3d50653f5ba15516448c07271d3cc72de1b7de7a
Eric Michael Taylor <etaylor1@bowser.ece.utexas.edu>
2022-04-24 18:20:13 -0500

    Full Fetch Decode Test

--- rtl/fetch/byte_shifter_32B.v
+++ rtl/fetch/byte_shifter_32B.v
@@ -20,7 +20,8 @@ module byte_shifter_32B (
       for(i = 0; i < 32; i=i+1) begin
 	 wire [4:0] sel;
 	 wire 	    nc;	 
-	 slow_addr #(.WIDTH(5)) shft_add (i, shift_amount, sel, nc);	 
+	 //slow_addr #(.WIDTH(5)) shft_add (i, shift_amount, sel, nc);
+	 assign sel = i - shift_amount;	 
          mux #(.WIDTH(8), .INPUTS(32)) byte_mux (in, out[((i+1)*8)-1:(i*8)],sel);	 
       end
    endgenerate
