cb29c34acfd122eb7778a3300625c2c7118d7c62
Eric Michael Taylor <etaylor1@bowser.ece.utexas.edu>
2022-05-04 23:52:41 -0500

    TB With Memory System Integrated Running

--- tb/tb_top/tb_mem_top/tb_reg_op.v
+++ tb/tb_top/tb_mem_top/tb_reg_op.v
@@ -92,8 +92,6 @@ module TOP;
    wire  [32-1:0] mem_data;
    wire           mem_rd_wr;
    wire           mem_en;
-
-   test_memory mem(clk, reset, mem_addr, mem_en, mem_data_valid, mem_data, mem_rd_wr);
    
    assign contents_concat = {
         contents[0],
@@ -293,7 +291,7 @@ module TOP;
        emem_ready        = ~ememory_valid;     
   end
    
-  always #10  clk          = ~clk;
+  always #5  clk          = ~clk;
 
   initial begin
         $vcdplusfile("fsys_reg.dump.vpd");
@@ -301,77 +299,3 @@ module TOP;
   end
 
 endmodule // TOP
-
-module test_memory(
-    clk,
-    reset,
-    addr,
-    en,
-    data_valid,
-    data,
-    rd_wr
-);
-    input clk;
-    input reset;
-
-    input [31:0] addr;
-    input en;
-    output data_valid;
-    output [31:0] data;
-    input rd_wr;
-
-    reg [31:0] memory [0:1023];
-
-    reg [31:0] out_data;
-
-    wire [31:0] 		rom_data_0, rom_data_1, rom_data_2, rom_data_3;
-
-    wire [31:0]			rom_data;
-
-    assign rom_data = (addr[3:2] == 3) ? rom_data_0 : 
-                      (addr[3:2] == 2) ? rom_data_1 :    
-                      (addr[3:2] == 1) ? rom_data_2 : rom_data_3;
- 
-    rom32b32w$ test_rom_0 (
-     addr[8:4],
-     1'b1,
-     rom_data_0		      
-    );
-
-    rom32b32w$ test_rom_1 (
-     addr[8:4],
-     1'b1,
-     rom_data_1		      
-    );
-   
-    rom32b32w$ test_rom_2 (
-     addr[8:4],
-     1'b1,
-     rom_data_2		      
-    );
-   
-    rom32b32w$ test_rom_3 (
-     addr[8:4],
-     1'b1,
-     rom_data_3		      
-    );       
-
-    //always @(posedge clk, negedge reset) begin
-    //
-    //
-    //end
-
-    //always @(posedge clk) begin
-    //    out_data <= addr;
-    //end
-    //
-    always @(*) begin
-        out_data = rom_data;
-    end
-
-    assign data_valid = 1'b1;
-    assign data = en ? out_data : 32'dz;
-
-
-endmodule
-
