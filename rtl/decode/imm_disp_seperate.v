ed95d45a0bf6c1b0a0d9b2fbfe808a7d914dde96
Eric Michael Taylor <etaylor1@bowser.ece.utexas.edu>
2022-05-11 08:42:29 -0500

    Sign Extend Fix

--- rtl/decode/imm_disp_seperate.v
+++ rtl/decode/imm_disp_seperate.v
@@ -12,20 +12,14 @@ input [3:0]     s0_immediete_bytes;  // 6, 4, 2, 1
 input [3:0]     s0_displacement_bytes;	// 4, 1, 0    
 output [47:0]   dec_imm;
 output [31:0]   dec_disp;
-   input [2:0] 	dec_size;
-   
+ input [2:0] 	dec_size;
 
-wire [47:0] 	imm_mask;
+   wire [47:0] 	imm_mask;
 wire [31:0] disp_mask;
 
 wire [47:0] 	imm;
-wire [31:0]    disp;
-   
-wire [47:0] 	imm_clean;
-wire [31:0]    disp_clean; 
-   
-  wire [47:0] 	imm_unextended;
-wire [31:0]    disp_unextended; 
+wire [31:0]    disp;   
+wire [31:0]    dec_disp_masked;   
 
 wire [63:0] s0_displace_n_imm_shift;
 
@@ -87,10 +81,10 @@ subtract #(.WIDTH(4)) (
 
   
 byte_shifter_right_6B (48'hFFFFFFFFFFFF, imm_mask, sub6_imm_bytes[2:0]); 
-byte_shifter_right_8B (end_swap_s0_displace_n_imm, imm_unextended, s0_displacement_bytes[2:0]);
+byte_shifter_right_8B (end_swap_s0_displace_n_imm, imm, s0_displacement_bytes[2:0]);
 
 byte_shifter_right_4B (32'hFFFFFFFF, disp_mask, sub4_disp_bytes[1:0]); 
-//byte_shifter_right_8B (end_swap_s0_displace_n_imm, disp, 0);   
+byte_shifter_right_8B (end_swap_s0_displace_n_imm, disp, 0);   
 
 // TODO - Make Case
 //assign imm_mask  = (48'hFFFFFFFFFFFF >> 8*(6-s0_immediete_bytes));
@@ -99,42 +93,39 @@ byte_shifter_right_4B (32'hFFFFFFFF, disp_mask, sub4_disp_bytes[1:0]);
 //assign imm  = (s0_displace_n_imm >> 8*(8-(s0_immediete_bytes+s0_displacement_bytes)));
 //assign disp = (s0_displace_n_imm >> 8*(8-s0_displacement_bytes));
 
-assign disp_unextended = end_swap_s0_displace_n_imm[31:0];
-
-//mux #(.WIDTH(48),.INPUTS(2)) ({{{40{imm_unextended[7]}},imm_unextended[7:0]},imm_unextended},  imm  ,one_byte_imm);
-//mux #(.WIDTH(32),.INPUTS(2)) ({{{24{disp_unextended[7]}},disp_unextended[7:0]},disp_unextended},  disp  ,one_byte_imm);  
-   
-logic_tree_bus #(.WIDTH(32),.NINPUTS(2)) disp_maskb ({disp_mask,disp_unextended},dec_disp_clean);
-logic_tree_bus #(.WIDTH(48),.NINPUTS(2)) imm_maskb  ({imm_mask,imm_unextended},dec_imm_clean);
-
-// Sign Extend
-
-wire        one_byte_imm, one_byte_disp, size_is_16, size_is_32, extend_to_32, extend_to_16;
-wire        extend_imm;
-wire        extend_disp;
+// Create Extend OR Vector   
+wire    sign_extend_4B_disp = {{24{end_swap_s0_displace_n_imm[7]}},8'b0};
   
+wire    sign_extend_4B_imm = {16'b0, {24{imm[7]}}, 8'b0};
+wire    sign_extend_2B_imm = {32'b0, {8{imm[7]}}, 8'b0};
+
+wire [47:0] sign_extend_imm; 
+wire [31:0] sign_extend_disp;
+wire [47:0] dec_disp_masked; 
+wire [31:0] dec_imm_masked;   
+wire [2:0]    masked_size;
+
+compare #(.WIDTH(4)) (4'd1, s0_immediete_bytes,  imm_byte_one);
+compare #(.WIDTH(4)) (4'd1, s0_displacement_bytes,  disp_byte_one);   
+
+logic_tree_bus #(.WIDTH(3),.NINPUTS(2))  ({~imm_byte_one,dec_size},masked_size);
+
+mux #(.WIDTH(32),.INPUTS(2)) ({sign_extend_disp,32'b0}, sign_extend_disp, disp_byte_one);
+mux #(.WIDTH(48),.INPUTS(2)) ({sign_extend_4B_imm,sign_extend_2B_imm,32'b0,32'b0}, sign_extend_imm, masked_size);
+
+wire [31:0] dec_disp_extend;
    
-compare #(.WIDTH(4)) (s0_immediete_bytes, 4'd1, one_byte_imm);
-compare #(.WIDTH(4)) (s0_displacement_bytes, 4'd1, one_byte_disp);
-   
-compare #(.WIDTH(3)) (dec_size, 3'd2, size_is_16);
-compare #(.WIDTH(3)) (dec_size, 3'd3, size_is_32);
+logic_tree_bus #(.WIDTH(32),.NINPUTS(2)) disp_maskb ({disp_mask,end_swap_s0_displace_n_imm[31:0]},dec_disp_masked);
+logic_tree_bus #(.WIDTH(48),.NINPUTS(2)) imm_maskb  ({imm_mask,imm},dec_imm_masked);
 
-and2$ (extend_to_32, size_is_32, one_byte_imm);
-and2$ (extend_to_16, size_is_16, one_byte_imm);
+logic_tree_bus #(.WIDTH(32),.NINPUTS(2),.OPERATION(1)) disp_ext ({sign_extend_disp,dec_disp_masked},dec_disp_extend);
+logic_tree_bus #(.WIDTH(48),.NINPUTS(2),.OPERATION(1)) imm_ext  ({sign_extend_imm,dec_imm_masked},dec_imm);   
 
-or2$ (extend_imm, extend_to_32, extend_to_16);
+// Possibly Zero out DISP		      
+mux #(.WIDTH(32),.INPUTS(2)) ({32'h0,dec_disp_extend}, dec_disp, disp_byte_zero);
 
-assign dec_4B_extended = {{24{disp_unextended[7]}},disp_unextended[7:0]};
-assign imm_4B_extended = {16'b0,{24{imm_unextended[7]}},imm_unextended[7:0]};
-assign imm_2B_extended = {32'b0,{8{imm_unextended[7]}},imm_unextended[7:0]};
-
-assign dec_disp = (one_byte_disp) ? dec_4B_extended : dec_disp_clean;
-assign dec_imm = (extend_to_32) ? imm_4B_extended :
-		 (extend_to_16) ? imm_2B_extended :
-                                  dec_imm_clean;
-
-   	  
+nor3$ (disp_byte_zero, s0_displacement_bytes[2], s0_displacement_bytes[1], s0_displacement_bytes[0]);
+	  
 endmodule
 
 ////////////////////////////////////////////
