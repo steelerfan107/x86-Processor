cd848cabf857753dc98388283c80395f68857525
Eric Michael Taylor <etaylor1@bowser.ece.utexas.edu>
2022-05-11 15:44:51 -0500

    PushPop Fixes

--- rtl/memory_subsystem/memory_subsystem_top.v
+++ rtl/memory_subsystem/memory_subsystem_top.v
@@ -123,7 +123,7 @@ module memory_subsystem_top (
     wire bus_en;
 
     wire bus_busy_icache;
-    wire bus_busy_dcache = 'h0;
+    wire bus_busy_dcache;
     wire bus_busy_sys_controller = 'h0;
     wire bus_busy_dma = 'h0;
     wire bus_busy;
@@ -174,7 +174,7 @@ module memory_subsystem_top (
     wire [31:0] tlb_d_pa;
     wire tlb_d_pcd;
 
-    wire d_virt_addr;
+    wire [31:0] d_virt_addr;
 
     TLB tlb(
         .contents(tlb_contents),   
