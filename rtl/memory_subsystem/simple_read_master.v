75e51f9cc4bb5ae8f1f9a2cf35ee404934fb2aa1
hbarclay <barclay.harrisonr@gmail.com>
2022-05-12 17:22:31 -0500

    simple read master fixed but untested

--- rtl/memory_subsystem/simple_read_master.v
+++ rtl/memory_subsystem/simple_read_master.v
@@ -39,6 +39,8 @@ module simple_read_master(
     input [31:0] mem_data;
     output mem_rd_wr;
     output mem_en;
+    assign dp_read_data = mem_data;
+    assign mem_rd_wr = 0;
 
     input grant_in;
     output grant_out;
@@ -59,6 +61,17 @@ module simple_read_master(
 
     wire reset_n;
 
+
+    wire [31:0] req_addr;
+    wire req_en;
+
+    register #(.WIDTH(32)) req_addr_reg(clk, reset, req_address, req_addr, , req_en);
+
+    wire mem_en_n;
+    inv1$ men(mem_en_n, mem_en);
+    tristate_bus_drive16$(mem_en_n, req_addr[15:0], mem_addr[15:0]);
+    tristate_bus_drive16$(mem_en_n, req_addr[31:16], mem_addr[31:16]);
+
     inv1$ reset_inv(reset_n, reset);
 
     dff$ 
@@ -71,28 +84,34 @@ module simple_read_master(
 
     wire state1_not;
     wire state0_not;
-    wire req_valid_not;
     wire and0;
     wire and1;
-    wire or0;
-    wire bus_busy_in_not;
+    wire req_valid_not;
     wire and2;
     wire and3;
-    wire or1;
+    wire or0;
+    wire bus_busy_in_not;
     wire and4;
     wire and5;
-    wire and6;
-    wire or2;
-    wire and7;
     wire dp_ready_not;
-    wire and8;
+    wire and6;
     wire mem_ready_not;
+    wire and7;
+    wire or1;
+    wire and8;
     wire and9;
-    wire or3;
     wire and10;
-    wire grant_in_not;
     wire and11;
     wire and12;
+    wire or2;
+    wire and13;
+    wire and14;
+    wire and15;
+    wire or3;
+    wire and16;
+    wire grant_in_not;
+    wire and17;
+    wire and18;
     wire or4;
 
     inv1$ state1_inv (.out(state1_not), .in(state1));
@@ -103,33 +122,40 @@ module simple_read_master(
     inv1$ mem_ready_inv (.out(mem_ready_not), .in(mem_ready));
     inv1$ grant_in_inv (.out(grant_in_not), .in(grant_in));
 
-    and3$ and_gate0(.out(and0), .in0(state1_not), .in1(state0_not), .in2(req_valid_not));
-    and3$ and_gate1(.out(and1), .in0(state1_not), .in1(state0_not), .in2(req_valid));
-    and4$ and_gate2(.out(and2), .in0(state1_not), .in1(state0), .in2(grant_in), .in3(bus_busy_in_not));
-    and2$ and_gate3(.out(and3), .in0(state1), .in1(state0_not));
-    and3$ and_gate4(.out(and4), .in0(state1_not), .in1(state0_not), .in2(req_valid_not));
-    and4$ and_gate5(.out(and5), .in0(state1_not), .in1(state0), .in2(grant_in), .in3(bus_busy_in_not));
-    and2$ and_gate6(.out(and6), .in0(state1), .in1(state0_not));
-    and4$ and_gate7(.out(and7), .in0(state1_not), .in1(state0), .in2(grant_in), .in3(bus_busy_in_not));
-    and3$ and_gate8(.out(and8), .in0(state1), .in1(state0_not), .in2(dp_ready_not));
-    and3$ and_gate9(.out(and9), .in0(state1), .in1(state0_not), .in2(mem_ready_not));
-    and3$ and_gate10(.out(and10), .in0(state1_not), .in1(state0), .in2(bus_busy_in));
-    and3$ and_gate11(.out(and11), .in0(state1_not), .in1(state0), .in2(grant_in_not));
-    and3$ and_gate12(.out(and12), .in0(state1_not), .in1(state0_not), .in2(req_valid));
+    and3$ and_gate0(.out(and0), .in0(state1_not), .in1(state0_not), .in2(req_valid));
+    and4$ and_gate1(.out(and1), .in0(state1), .in1(state0_not), .in2(mem_ready), .in3(dp_ready));
+    and3$ and_gate2(.out(and2), .in0(state1_not), .in1(state0_not), .in2(req_valid_not));
+    and3$ and_gate3(.out(and3), .in0(state1_not), .in1(state0_not), .in2(req_valid));
+    and4$ and_gate4(.out(and4), .in0(state1_not), .in1(state0), .in2(grant_in), .in3(bus_busy_in_not));
+    and4$ and_gate5(.out(and5), .in0(state1), .in1(state0_not), .in2(mem_ready), .in3(dp_ready));
+    and3$ and_gate6(.out(and6), .in0(state1), .in1(state0_not), .in2(dp_ready_not));
+    and3$ and_gate7(.out(and7), .in0(state1), .in1(state0_not), .in2(mem_ready_not));
+    and3$ and_gate8(.out(and8), .in0(state1_not), .in1(state0_not), .in2(req_valid_not));
+    and4$ and_gate9(.out(and9), .in0(state1_not), .in1(state0), .in2(grant_in), .in3(bus_busy_in_not));
+    and4$ and_gate10(.out(and10), .in0(state1), .in1(state0_not), .in2(mem_ready), .in3(dp_ready));
+    and3$ and_gate11(.out(and11), .in0(state1), .in1(state0_not), .in2(dp_ready_not));
+    and3$ and_gate12(.out(and12), .in0(state1), .in1(state0_not), .in2(mem_ready_not));
+    and4$ and_gate13(.out(and13), .in0(state1_not), .in1(state0), .in2(grant_in), .in3(bus_busy_in_not));
+    and3$ and_gate14(.out(and14), .in0(state1), .in1(state0_not), .in2(dp_ready_not));
+    and3$ and_gate15(.out(and15), .in0(state1), .in1(state0_not), .in2(mem_ready_not));
+    and3$ and_gate16(.out(and16), .in0(state1_not), .in1(state0), .in2(bus_busy_in));
+    and3$ and_gate17(.out(and17), .in0(state1_not), .in1(state0), .in2(grant_in_not));
+    and3$ and_gate18(.out(and18), .in0(state1_not), .in1(state0_not), .in2(req_valid));
 
-    or2$ or_gate0(.out(or0), .in0(and0), .in1(and1));
-    or2$ or_gate1(.out(or1), .in0(and2), .in1(and3));
-    or2$ or_gate2(.out(or2), .in0(and5), .in1(and6));
-    or3$ or_gate3(.out(or3), .in0(and7), .in1(and8), .in2(and9));
-    or3$ or_gate4(.out(or4), .in0(and10), .in1(and11), .in2(and12));
+    or2$ or_gate0(.out(or0), .in0(and2), .in1(and3));
+    or4$ or_gate1(.out(or1), .in0(and4), .in1(and5), .in2(and6), .in3(and7));
+    or4$ or_gate2(.out(or2), .in0(and9), .in1(and10), .in2(and11), .in3(and12));
+    or3$ or_gate3(.out(or3), .in0(and13), .in1(and14), .in2(and15));
+    or3$ or_gate4(.out(or4), .in0(and16), .in1(and17), .in2(and18));
 
+    assign req_en = and0;
+    assign dp_valid = and1;
     assign req_ready = or0;
     assign mem_req = or1;
-    assign grant_pass = and4;
+    assign grant_pass = and8;
     assign mem_en = or2;
     assign new_state1 = or3;
     assign new_state0 = or4;
-
-        
+            
 
 endmodule
