4f2f895c5a30d6b84418d8f701eeffd79aa826d4
Brandon Zupan <zupan.brandon@utexas.edu>
2022-05-13 14:09:11 -0500

    Set mmx to start with Z

--- rtl/register_access/mmx_register.v
+++ rtl/register_access/mmx_register.v
@@ -81,6 +81,26 @@ module mmx_register_file (      // fanout good
     mm5 (clk, reset, writeback_data_buf, mm5_out, ,wb_decoded_enabled[5]),
     mm6 (clk, reset, writeback_data_buf, mm6_out, ,wb_decoded_enabled[6]),
     mm7 (clk, reset, writeback_data_buf, mm7_out, ,wb_decoded_enabled[7]);
+
+    // 16 32 bit registers that are initialized to Z when reset
+    register_32_reset 
+    mm0_high (mm0_out[63:32], writeback_data_buf[63:32], , wb_decoded_enabled[0], clk, reset), 
+    mm1_high (mm1_out[63:32], writeback_data_buf[63:32], , wb_decoded_enabled[1], clk, reset), 
+    mm2_high (mm2_out[63:32], writeback_data_buf[63:32], , wb_decoded_enabled[2], clk, reset), 
+    mm3_high (mm3_out[63:32], writeback_data_buf[63:32], , wb_decoded_enabled[3], clk, reset), 
+    mm4_high (mm4_out[63:32], writeback_data_buf[63:32], , wb_decoded_enabled[4], clk, reset), 
+    mm5_high (mm5_out[63:32], writeback_data_buf[63:32], , wb_decoded_enabled[5], clk, reset), 
+    mm6_high (mm6_out[63:32], writeback_data_buf[63:32], , wb_decoded_enabled[6], clk, reset), 
+    mm7_high (mm7_out[63:32], writeback_data_buf[63:32], , wb_decoded_enabled[7], clk, reset), 
+    mm0_low (mm0_out[31:0], writeback_data_buf[31:0], , wb_decoded_enabled[0], clk, reset), 
+    mm1_low (mm1_out[31:0], writeback_data_buf[31:0], , wb_decoded_enabled[1], clk, reset), 
+    mm2_low (mm2_out[31:0], writeback_data_buf[31:0], , wb_decoded_enabled[2], clk, reset), 
+    mm3_low (mm3_out[31:0], writeback_data_buf[31:0], , wb_decoded_enabled[3], clk, reset), 
+    mm4_low (mm4_out[31:0], writeback_data_buf[31:0], , wb_decoded_enabled[4], clk, reset), 
+    mm5_low (mm5_out[31:0], writeback_data_buf[31:0], , wb_decoded_enabled[5], clk, reset), 
+    mm6_low (mm6_out[31:0], writeback_data_buf[31:0], , wb_decoded_enabled[6], clk, reset), 
+    mm7_low (mm7_out[31:0], writeback_data_buf[31:0], , wb_decoded_enabled[7], clk, reset);
+
     
 
 endmodule
