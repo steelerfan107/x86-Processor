edb7227622464cbc2bee4091b122a2ec45303efb
Brandon Zupan <zupan.brandon@utexas.edu>
2022-05-12 21:55:30 -0500

    Fix 8 bit move

--- tb/tb_top/tb_mem_top/tb_reg_op.v
+++ tb/tb_top/tb_mem_top/tb_reg_op.v
@@ -189,7 +189,7 @@ module TOP;
    reg 		r_emem_valid;
    reg 		r_r_emem_valid;
   
-   assign wmem_wr_size = 'h0; //wmem_wr_size_nc - 1; 
+   // assign wmem_wr_size = 'h0; //wmem_wr_size_nc - 1; 
    top_pipeline #(.SINGLE_TXN(SINGLE_TXN)) uut_pipeline(
       clk,
       reset,
@@ -231,7 +231,7 @@ module TOP;
       wmem_address,
       wmem_wr_en,
       wmem_wr_data,
-      wmem_wr_size_nc,
+      wmem_wr_size,
       wmem_dp_valid,
       wmem_dp_ready,
       wmem_dp_read_data  		     
